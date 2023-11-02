b5d5c49dbd9169b9c462b34b84f492765170614b
jmatthews64 <jon.matthews64@gmail.com>
2023-11-02 11:50:53 -0400

    Created a function to open the file and choose a random word from it

--- mystery_word.py
+++ mystery_word.py
@@ -1,6 +1,38 @@
-def play_game():
-    pass
+import random
 
+ALPHABET_LIST = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+
+# This function selects a word at random from the file it is passed
+def choose_random_word(file):
+    # open and read the file
+    with open (file, 'r') as reader:
+        open_doc = reader.read()
+    # with new string convert it to a list of strings all upper case
+    new_list = open_doc.upper().split()
+    # print(new_list)
+    # choose a random word based on the index in the new list
+    answer = new_list[random.randint(0, len(new_list))]
+    return answer
+
+
+def play_game(file):
+    answer = choose_random_word(file)
+    print(answer)
 
 if __name__ == "__main__":
-    play_game()
+    import argparse
+    from pathlib import Path
+
+    parser = argparse.ArgumentParser(
+        description='Get the word frequency in a text file.')
+    parser.add_argument('file', help='file to read')
+    args = parser.parse_args()
+
+    file = Path(args.file)
+    if file.is_file():
+        play_game(file)
+    else:
+        print(f"{file} does not exist!")
+        exit(1)
+    
